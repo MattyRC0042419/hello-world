8234c59741392effbedd0422b19bd9751a87c2c9
Matthew Waugh <RC0042419@student.riversidecollege.ac.uk>
2021-03-15 15:23:41 +0000

    Added hello button

--- hello world/Form1.cs
+++ hello world/Form1.cs
@@ -19,7 +19,7 @@ namespace hello_world
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World");
+            MessageBox.Show("Hello " + txtBxName.Text);
 
         }
     }
